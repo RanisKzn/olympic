38694d8bc241ba5aae3cd8fea7245b1fe6f127d6
RanisKzn <77621853+RanisKzn@users.noreply.github.com>
2021-09-20 22:34:23 +0300

    Add files via upload

---
+++ Olympic/Olympic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olympic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mylist = new List<string>() { "pasha","sssss","anna","masha","pasha", "masha", "masha", "anna", "anna", "anna" };
+            var dict = new Dictionary<string, int>();
+
+            for (int i = 0; i < mylist.Count; i++)
+            {
+                if(dict.ContainsKey(mylist[i]))
+                {
+                    dict[mylist[i]] += 1;
+                }
+                else
+                {
+                    dict.Add(mylist[i], 1);
+                }
+            }
+            foreach(var item in dict.OrderByDescending (item => item.Value))
+            {
+                
+                Console.WriteLine( "{0} - {1}",item.Key, item.Value) ;
+            }
+        }
+    }
+}
